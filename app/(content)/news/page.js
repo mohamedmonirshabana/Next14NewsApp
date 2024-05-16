c904a5c04524024e7fac4c7ffbba208b8a97d670
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2024-05-16 13:21:47 +0300

    remove last thing for client side to work on Server Component

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,16 +1,17 @@
 import NewsList from '@/components/news-list';
 export default async function NewsPage() {
   const response = await fetch('http://localhost:8080/news');
+
+  if (!response.ok) {
+    throw new Error('Fail to fetch news.');
+  }
+
   const news = await response.json();
 
-  let newsContent;
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 }
